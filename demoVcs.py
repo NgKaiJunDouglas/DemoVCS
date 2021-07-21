58d8eeabe151db58ad165b96520f1f83aabeaa63
Douglas <douglas10pvp@gmail.com>
2021-07-21 12:01:47 +0800

    DemoVcs

---
+++ demoVcs.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(num1, num2):
+        sum=num1+num2
+        print("sum is" ,sum)
+    def minus(num1, num2):
+        min=num1-num2
+        print("sum is" ,min)
+
+Calculator.add(50,40)
+Calculator.minus(50,40)
+
+
